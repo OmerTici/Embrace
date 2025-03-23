17a9693d93def9dfd254b6ba95bffc32600723f3
Omer Tici <ticiomer@gmail.com>
2025-03-23 16:10:28 -0400

    new flyer

--- src/components/Events.js
+++ src/components/Events.js
@@ -34,7 +34,7 @@ function Events() {
       {/* UPCOMING EVENT */}
       <div className="upcoming-event">
         <img
-          src="/Embrace/March28th_flyer_visual_2.png"
+          src="/Embrace/flyer_wnewdj.png"
           alt="Upcoming Event Poster"
           className="big-poster"
         />
